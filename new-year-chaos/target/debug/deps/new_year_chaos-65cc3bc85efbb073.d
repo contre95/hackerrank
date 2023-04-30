6de1f4d003e6c4764cee82d5dd90445e3ead0e73
contre95 <lucascontre95@gmail.com>
2023-05-01 00:43:46 +0200

    Rust

--- new-year-chaos/target/debug/deps/new_year_chaos-65cc3bc85efbb073.d
+++
@@ -1,8 +0,0 @@
-/home/canus/Code/ContreCode/hackerrank/new-year-chaos/target/debug/deps/new_year_chaos-65cc3bc85efbb073.rmeta: src/main.rs Cargo.toml
-
-/home/canus/Code/ContreCode/hackerrank/new-year-chaos/target/debug/deps/new_year_chaos-65cc3bc85efbb073.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
--- new-year-chaos/target/debug/new-year-chaos.d
+++
@@ -1,1 +0,0 @@
-/home/canus/Code/ContreCode/hackerrank/new-year-chaos/target/debug/new-year-chaos: /home/canus/Code/ContreCode/hackerrank/new-year-chaos/src/main.rs
