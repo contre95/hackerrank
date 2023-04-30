30182e4219f8b8a180c8b299ab542a1459cd6b1f
contre95 <lucascontre95@gmail.com>
2023-05-01 00:59:35 +0200

    Rust

--- new-year-chaos/src/main.rs
+++ new-year-chaos/src/main.rs
@@ -2,19 +2,25 @@ fn minimum_bribes(q: &[i32]) {
     let mut count: usize = 0;
     let mut pivot: i32 = 0;
     while count < q.len() {
-        let a = (q[count] - 1) - (count as i32);
+        let a = q[count] - (count as i32) - 1;
+        // println!("{}", a);
         if a > 2 {
             println!("Too chaotic");
             return;
-        } else if a < 0 {
+        } else if a > 0 {
             pivot += a;
         }
         count += 1;
-    println!("{}", pivot);
     }
+    println!("{}", pivot);
 }
 
 fn main() {
     let q = [1, 2, 5, 3, 7, 8, 6, 4];
     minimum_bribes(&q);
+    // let q = [2, 1, 5, 3, 4];
+    // minimum_bribes(&q);
+    // let q = [2, 5, 1, 3, 4];
+    // minimum_bribes(&q);
 }
+
