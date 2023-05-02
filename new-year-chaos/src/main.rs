f83d8fb3a85f7c7e0f2626abb710f36c999f1a4d
contre95 <lucascontre95@gmail.com>
2023-05-02 16:07:15 +0200

    [INFRASEC-3171] A ver si anda

--- new-year-chaos/src/main.rs
+++ new-year-chaos/src/main.rs
@@ -3,24 +3,31 @@ fn minimum_bribes(q: &[i32]) {
     let mut pivot: i32 = 0;
     while count < q.len() {
         let a = q[count] - (count as i32) - 1;
-        // println!("{}", a);
         if a > 2 {
             println!("Too chaotic");
             return;
-        } else if a > 0 {
-            pivot += a;
         }
+        let mut count2 = count;
+        while count2 < q.len() {
+            if q[count2] < q[count] {
+                // pivot += 1
+            }
+            count2+=1;
+        }
+        println!(".  {}-{}-{}", count + 1, q[count], a);
         count += 1;
     }
     println!("{}", pivot);
 }
 
-fn main() {
-    let q = [1, 2, 5, 3, 7, 8, 6, 4];
-    minimum_bribes(&q);
-    // let q = [2, 1, 5, 3, 4];
-    // minimum_bribes(&q);
-    // let q = [2, 5, 1, 3, 4];
-    // minimum_bribes(&q);
-}
 
+fn main() {
+    let q = [1, 2, 5, 3, 4, 7, 8, 6]; // 4
+    minimum_bribes(&q);
+    let q = [1, 2, 5, 3, 7, 8, 6, 4]; // 7
+    minimum_bribes(&q);
+    let q = [2, 1, 5, 3, 4]; // 3
+    minimum_bribes(&q);
+    let q = [2, 5, 1, 3, 4]; // Too chaotic
+    minimum_bribes(&q);
+}
